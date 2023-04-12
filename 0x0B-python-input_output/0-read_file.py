1089d7ea907c8c9f2dd3ad6acace162085812665
Sjallow <seringj54@gmail.com>
2023-04-12 00:18:42 -0700

    python in out

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+ a text file-reading function."""
+
+
+ def read_file(filename=""):
+     """Read a text file and output its contents to stdout"""
+with open(filename, 'r', encoding='utf-8') as f:            print(f.read(), end="")
