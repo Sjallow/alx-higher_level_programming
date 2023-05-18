9ba48f79b3d4cc43aa8692a93997fddfca68aec1
Sjallow <seringj54@gmail.com>
2023-05-17 18:48:28 -0700

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
-sts all rows of a database corresponding to a column value
+-- lists all rows of a database corresponding to a column value
 SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
