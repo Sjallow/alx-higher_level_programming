e955fa768aef52e46683ecdee7066595b89113e3
Sjallow <seringj54@gmail.com>
2023-05-17 18:45:06 -0700

    sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
---lsts all rows of a column in a database
+-- lists all rows of a column in a database
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
