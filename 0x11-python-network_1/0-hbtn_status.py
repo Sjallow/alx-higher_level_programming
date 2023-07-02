5c51724d1dc0246a36fefff1ec11abd886d80023
Sjallow <seringj54@gmail.com>
2023-07-02 05:00:00 -0700

    py networks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""This script makes a request to the url and gets a response"""
-from urllib.request import urlopen, Request
-from urllib.error import URLError
+"""
+A Python script that:
+* fetches https://alx-intranet.hbtn.io/status,
+* uses the 'urlib' package.
+"""
 
 
-def get_body_decode():
-    try:
-        req = Request("https://alx-intranet.hbtn.io/status")
-        with urlopen(req) as response:
-            body = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except URLError as e:
-        if hasattr(e, "reason"):
-            print(e.reason)
-        elif hasattr(e, "code"):
-            print(e.code)
-
-
-if __name__ == "__main__":
-    get_body_decode()
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
